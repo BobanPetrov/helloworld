aa7138bb7d049c58d63114add7b93671b7fbe4d4
Boban Petrov <netpetrov@gmail.com>
2020-02-14 10:36:46 +0100

    Initialized project

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Boban Petrov on 2/14/20.
+//  Copyright © 2020 Boban Petrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
